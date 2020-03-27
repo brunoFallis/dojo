70fa9687f816ec26ae292673c784c344dfb103f6
Matheus Martins <cin_masilva@uolinc.com>
2020-03-26 23:02:25 -0300

    project-one: create list all users

--- project-one/src/controllers/UserController.js
+++ project-one/src/controllers/UserController.js
@@ -2,15 +2,22 @@ const User = require('../models/User')
 
 module.exports = {
 
-    save(request, response) {
+    async get(request, response) {
+
+        const users = await User.find();
+
+        return response.json(users).status(200)
+    },
+
+    async save(request, response) {
         const { userName, userAge } = request.body;
 
         if (!userName || !userAge)
             return response.send().status(400)
 
-        User.create( { name: userName, age: userAge } )
+        const user = await User.create({ name: userName, age: userAge })
 
-        return response.json(request.body).status(200)
+        return response.json(user).status(200)
     }
 
 }
--- project-one/src/routes.js
+++ project-one/src/routes.js
@@ -9,6 +9,7 @@ routes.get('/', function(req, res) {
     res.sendFile(path.join(`${__dirname}/index.html`))
 })
 
+routes.get('/user', UserController.get);
 routes.post('/user', UserController.save);
 
 module.exports = routes;
