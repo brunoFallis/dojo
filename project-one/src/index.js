9506eb7da3ec447da9ccaddda587bd8f8e7ced74
Matheus Martins <cin_masilva@uolinc.com>
2020-03-26 22:36:46 -0300

    project-one: configure nodemon and mongoose

--- project-one/src/index.js
+++ project-one/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const routes = require('./routes')
 
+mongoose.connect('mongodb+srv://semana:semana@cluster0-w455j.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
 app.use(express.json())
 app.use(routes)
- 
+
 app.listen(3000, () => {
   console.log(`Server listening on port 3000`)
 })
